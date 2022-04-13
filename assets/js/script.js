437b9bdeecd7c2189af9c40180e1c62ed45d4538
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-04-13 18:17:59 -0400

    change program to roll a die

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,124 @@
 // TODO: Declare any global variables we need
-let totalFlips = 0
-let numHead = 0
-let percentHead = 0
-let numTail = 0
-let percentTail = 0
-let filpButton = document.querySelector("#flipButton")
-let clearButton = document.querySelector("#clearButton")
-let coinImg = document.querySelector("#coinImg")
+let totalRolls = 0
+let num1 = 0
+let percent1 = 0
+let num2 = 0
+let percent2 = 0
+let num3 = 0
+let percent3 = 0
+let num4 = 0
+let percent4 = 0
+let num5 = 0
+let percent5 = 0
+let num6 = 0
+let percent6 = 0
+let rollButton = document.querySelector("#roll")
+let clearButton = document.querySelector("#clear")
+let diceImg = document.querySelector("#diceImg")
 let statusMsg = document.querySelector("#statusMsg")
-let isHead = true
+let outcome
 
 
 
     // TODO: Add event listener and handler for flip and clear buttons
-    filpButton.addEventListener("click",function(){
-        filpCoin()
+    rollButton.addEventListener("click",function(){
+        rollDice()
     })
     clearButton.addEventListener("click",function(){
         clearScoreboard()
     })
     // Flip Button Click Handler
-    function filpCoin(){
-        isHead = Math.round(Math.random() * 100);
+    function rollDice(){
+        outcome = Math.ceil(Math.random() * 6);
         
-        if(isHead % 2 === 0){
-            coinImg.setAttribute("src", "./assets/images/penny-heads.jpg");
-            statusMsg.textContent = "You Flipped Heads";
-        }else{
-            coinImg.setAttribute("src", "./assets/images/penny-tails.jpg");
-            statusMsg.textContent = "You Flipped Tails";
+        switch(outcome){
+            case 1:
+                diceImg.setAttribute("src", "./assets/images/dice/dice1.png");
+                statusMsg.textContent = "You Rolled a 1";
+                break;
+            case 2:
+                diceImg.setAttribute("src", "./assets/images/dice/dice2.png");
+                statusMsg.textContent = "You Rolled a 2";
+                break;
+            case 3:
+                diceImg.setAttribute("src", "./assets/images/dice/dice3.png");
+                statusMsg.textContent = "You Rolled a 3";
+                break;
+            case 4:
+                diceImg.setAttribute("src", "./assets/images/dice/dice4.png");
+                statusMsg.textContent = "You Rolled a 4";
+                break;
+            case 5:
+                diceImg.setAttribute("src", "./assets/images/dice/dice5.png");
+                statusMsg.textContent = "You Rolled a 5";
+                break;
+            case 6:
+                diceImg.setAttribute("src", "./assets/images/dice/dice6.png");
+                statusMsg.textContent = "You Rolled a 6";
+                break;  
         }
-        calculateTotals(isHead);
-        updateScoreboard(numHead,percentHead,numTail,percentTail);
+        calculateTotals(outcome);
+        updateScoreboard();
     }
-    function calculateTotals(isHead){
-        totalFlips++
-        if(isHead % 2 === 0){
-            numHead++
-        }else{
-            numTail++
+    function calculateTotals(rolledValue){
+        totalRolls++
+        switch(rolledValue){
+            case 1:
+                num1++
+                break;
+            case 2:
+                num2++
+                break;
+            case 3:
+                num3++
+                break;
+            case 4:
+                num4++
+                break;
+            case 5:
+                num5++
+                break;
+            case 6:
+                num6++
+                break;  
         }
-        percentHead = Math.round(numHead/totalFlips * 100)
-        percentTail = Math.round(numTail/totalFlips * 100)
+        percent1 = Math.round(num1/totalRolls * 100)
+        percent2 = Math.round(num2/totalRolls * 100)
+        percent3 = Math.round(num3/totalRolls * 100)
+        percent4 = Math.round(num4/totalRolls * 100)
+        percent5 = Math.round(num5/totalRolls * 100)
+        percent6 = Math.round(num6/totalRolls * 100)
     }
-    function updateScoreboard(heads, headsPercent, tails, tailsPercent,){
-            document.querySelector("#heads").textContent = heads
-            document.querySelector("#tails").textContent = tails
-            document.querySelector("#heads-percent").textContent =`${headsPercent}%`
-            document.querySelector("#tails-percent").textContent =`${tailsPercent}%`
+    function updateScoreboard(){
+            document.querySelector("#ones").textContent = num1
+            document.querySelector("#twos").textContent = num2
+            document.querySelector("#threes").textContent = num3
+            document.querySelector("#fours").textContent = num4
+            document.querySelector("#fives").textContent = num5
+            document.querySelector("#sixes").textContent = num6
+            document.querySelector("#ones-percent").textContent =`${percent1}%`
+            document.querySelector("#twos-percent").textContent =`${percent2}%`
+            document.querySelector("#threes-percent").textContent =`${percent3}%`
+            document.querySelector("#fours-percent").textContent =`${percent4}%`
+            document.querySelector("#fives-percent").textContent =`${percent5}%`
+            document.querySelector("#sixes-percent").textContent =`${percent6}%`
     }
     function clearScoreboard(){
-        totalFlips = 0
-        numHead = 0
-        numTail = 0
-        percentHead = 0
-        percentTail = 0
-        statusMsg.textContent = "Lets Get Flipping!";
-        updateScoreboard(numHead,percentHead,numTail,percentTail)
+        totalRolls = 0 
+        num1 = 0
+        percent1 = 0
+        num2 = 0
+        percent2 = 0
+        num3 = 0
+        percent3 = 0
+        num4 = 0
+        percent4 = 0
+        num5 = 0
+        percent5 = 0
+        num6 = 0
+        percent6 = 0
+        statusMsg.textContent = "Lets Get Rolling!";
+        updateScoreboard()
     }
         // TODO: Determine flip outcome
         // TODO: Update image and status message in the DOM
